8958f0c1544cd56480ddb3fabfbb3d21ad3627a1
xiinmewing <rayyanxiin@gmail.com>
2026-07-03 00:55:31 +0700

    webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,1 +1,9 @@
+const bot = require('../bot');
 
+module.exports = async (req, res) => {
+  try {
+    await bot.handleUpdate(req.body, res);
+  } catch (error) {
+    res.status(200).send('OK');
+  }
+};
